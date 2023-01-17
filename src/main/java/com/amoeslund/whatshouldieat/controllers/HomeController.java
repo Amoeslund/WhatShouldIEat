29844833dfbcf742f02389b0a8448bbe857e7fff
Anders Moeslund <andersmoeslund@gmail.com>
2023-01-17 11:27:19 +0100

    configure gcloud

---
+++ src/main/java/com/amoeslund/whatshouldieat/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.amoeslund.whatshouldieat.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String Index() {
+        return "Hello";
+    }
+}
