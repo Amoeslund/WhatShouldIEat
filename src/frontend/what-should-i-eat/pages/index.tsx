f8a174b916817577feb9888dd0914c1495378c9b
Anders Moeslund <andersmoeslund@gmail.com>
2023-01-18 21:41:28 +0100

    fix title

--- src/frontend/what-should-i-eat/pages/index.tsx
+++ src/frontend/what-should-i-eat/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>What should I eat</title>
+                <title>Hvad Skal Jeg Spise</title>
                 <meta name="description" content="Never look through recipe sites again!"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
